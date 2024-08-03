f7d9fa76a812f04b1d78e44621a54c73fd316776
meatwad787 <greg_mrjohnson@yahoo.com>
2024-08-03 01:39:39 -0400

    Add constant variables for every button

---
+++ script.js
@@ -0,0 +1,7 @@
+const NumberButton = document.querySelectorAll('[data-number]');
+const OperationButton = document.querySelectorAll('[data-operation]');
+const DeleteButton = document.querySelectorAll('[data-delete]');
+const AllClearButton = document.querySelectorAll('[data-all-clear]');
+const EqualsButton = document.querySelectorAll('[data-equals]');
+const Previous = document.querySelectorAll('[data-previous]');
+const Current = document.querySelectorAll('[data-current]');
