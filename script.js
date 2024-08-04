eba9f89200456b4a4db14c5b6ded763b0cd6cf2b
meatwad787 <greg_mrjohnson@yahoo.com>
2024-08-04 09:32:24 -0400

    Create calculator class

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+class Calculator {
+    constructor (previous, current) {
+        this.previous = previous;
+        this.current = current;
+    }
+}
+
 const NumberButton = document.querySelectorAll('[data-number]');
 const OperationButton = document.querySelectorAll('[data-operation]');
-const DeleteButton = document.querySelectorAll('[data-delete]');
-const AllClearButton = document.querySelectorAll('[data-all-clear]');
-const EqualsButton = document.querySelectorAll('[data-equals]');
-const Previous = document.querySelectorAll('[data-previous]');
-const Current = document.querySelectorAll('[data-current]');
+const DeleteButton = document.querySelector('[data-delete]');
+const AllClearButton = document.querySelector('[data-all-clear]');
+const EqualsButton = document.querySelector('[data-equals]');
+const Previous = document.querySelector('[data-previous]');
+const Current = document.querySelector('[data-current]');
