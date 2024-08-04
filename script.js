397ea48ef44053599ee18c94701bb7a1e3991f52
meatwad787 <greg_mrjohnson@yahoo.com>
2024-08-04 09:59:19 -0400

    Add functions to carry out calculator actions

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-class Calculator {
-    constructor (previous, current) {
-        this.previous = previous;
-        this.current = current;
-    }
-}
-
 const NumberButton = document.querySelectorAll('[data-number]');
 const OperationButton = document.querySelectorAll('[data-operation]');
 const DeleteButton = document.querySelector('[data-delete]');
@@ -12,3 +5,34 @@ const AllClearButton = document.querySelector('[data-all-clear]');
 const EqualsButton = document.querySelector('[data-equals]');
 const Previous = document.querySelector('[data-previous]');
 const Current = document.querySelector('[data-current]');
+
+class Calculator {
+    constructor (previous, current) {
+        this.previous = previous;
+        this.current = current;
+    }
+
+    delete() {
+        
+    }
+
+    clear() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
