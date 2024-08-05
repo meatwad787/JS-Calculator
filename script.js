9292324ed43eefacc587319c00a36008627e937c
meatwad787 <greg_mrjohnson@yahoo.com>
2024-08-05 09:42:54 -0400

    Remove class
    add normal math functions
    also add event listener to append display with number button clicked

--- script.js
+++ script.js
@@ -6,33 +6,49 @@ const EqualsButton = document.querySelector('[data-equals]');
 const Previous = document.querySelector('[data-previous]');
 const Current = document.querySelector('[data-current]');
 
-class Calculator {
-    constructor (previous, current) {
-        this.previous = previous;
-        this.current = current;
-    }
+// Addition function
+function add(Previous, Current) {
+    return Previous + Current;
+}
 
-    delete() {
-        
-    }
+// Subtraction function
+function subtract(Previous, Current) {
+    return Previous - Current;
+}
 
-    clear() {
+// Multiplication function
+function multiply(Previous, Current) {
+    return Previous * Current;
+}
 
-    }
-
-    appendNumber(number) {
-
-    }
-
-    chooseOperation(operation) {
-
-    }
-
-    compute() {
-
-    }
-
-    updateDisplay() {
-
+// Division function
+function divide(Previous, Current) {
+    if (b !== 0) {
+        return Previous / Current;
+    } else {
+        return "Error: Division by zero";
     }
 }
+
+// Function to perform arithmetic operation based on operator
+function calculate(Previous, operator, Current) {
+    switch(operator) {
+        case '+':
+            return add(Previous, Current);
+        case '-':
+            return subtract(Previous, Current);
+        case '*':
+            return multiply(Previous, Current);
+        case '/':
+            return divide(Previous, Current);
+        default:
+            return "Invalid operator";
+    }
+}
+
+NumberButton.forEach(function(button) {
+    button.addEventListener("click", function() {
+        // Append the button's value to the input field
+        Current.innerHTML += button.textContent;
+    }
+    )});
